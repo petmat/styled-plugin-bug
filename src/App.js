c9f77e96edf32fda9071ebaebe2023b851c2901a
Matti Petrelius <matti.petrelius@gmail.com>
2019-04-13 12:09:12 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import styled, { css, ThemeProvider } from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const ClickableIcon = css`
+  cursor: pointer;
+`;
+
+const ClickableTreeIcon = css`
+  ${ClickableIcon}
+
+  width: 32px;
+  height: 32px;
+  background-image: url('${props => props.theme.paths.imgPath}/tree/tree.png');
+`;
+
+const MyIcon = styled.i`
+  ${ClickableTreeIcon}
+`;
+
+const theme = {
+  paths: { imgPath: '/imgs' }
+};
 
 class App extends Component {
   render() {
@@ -19,6 +40,9 @@ class App extends Component {
           >
             Learn React
           </a>
+          <ThemeProvider theme={theme}>
+            <MyIcon />
+          </ThemeProvider>
         </header>
       </div>
     );
